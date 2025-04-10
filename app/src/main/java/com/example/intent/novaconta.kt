2702d948659184d92225cc1aa548094d56f1d2b8
kotlinpj <kotlinpj.23@gmail.com>
2025-04-10 13:59:24 -0300

    inclusao de estilo no text view de termos de uso

--- app/src/main/java/com/example/intent/novaconta.kt
+++ app/src/main/java/com/example/intent/novaconta.kt
@@ -1,6 +1,12 @@
 package com.example.intent
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,33 @@ class novaconta : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tv_termos_uso: TextView = findViewById(R.id.tv_termos)
+        val textoCompleto = tv_termos_uso.text.toString()
+        val trechoNegrito = "Termos de Uso"
+
+        val spannable = SpannableString(textoCompleto)
+
+        val start = textoCompleto.indexOf(trechoNegrito)
+        val end = start + trechoNegrito.length
+
+        if (start >= 0) {
+            spannable.setSpan(
+                StyleSpan(Typeface.BOLD), // negrito
+                start,
+                end,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            spannable.setSpan(
+                UnderlineSpan(),
+                start,
+                end,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        tv_termos_uso.text = spannable
     }
+
 }
